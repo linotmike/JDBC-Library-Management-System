e3ffccf376a0bdb87318639a07745b8c2972a1d8
linotmike <128604219+linotmike@users.noreply.github.com>
2024-12-07 11:57:23 -0800

    Added properties,constructor,getters and setters to the library class

--- src/main/java/com/ps/models/Library.java
+++ src/main/java/com/ps/models/Library.java
@@ -1,4 +1,58 @@
 package com.ps.models;
 
 public class Library {
+    private int library_id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Library(){}
+
+    public Library(int library_id,String name, String address, String phone){
+        this.library_id = library_id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public int getLibrary_id(){
+        return this.library_id;
+    }
+    public void setLibrary_id(int library_id){
+        this.library_id = library_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "library_id=" + library_id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
