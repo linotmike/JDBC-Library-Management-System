95f1efb203deaf980b798d03df0cbd97514a8575
linotmike <128604219+linotmike@users.noreply.github.com>
2024-12-07 12:15:35 -0800

    Added properties,constructor,getters and setters to the patron class

--- src/main/java/com/ps/models/Patron.java
+++ src/main/java/com/ps/models/Patron.java
@@ -1,4 +1,58 @@
 package com.ps.models;
 
 public class Patron {
+    private int patron_id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public Patron(){}
+
+    public Patron (int patron_id,String name,String email,String phone){
+        this.patron_id = patron_id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getPatron_id (){
+        return this.patron_id;
+    }
+    public void setPatron_id(int patron_id){
+        this.patron_id = patron_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Patron{" +
+                "patron_id=" + patron_id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
